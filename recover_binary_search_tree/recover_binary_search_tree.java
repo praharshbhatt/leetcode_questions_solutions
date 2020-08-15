21c7dda0fa0ebd40dec67ec6cb8daa8940c2881e
praharsh bhatt <prahu.bhatt@gmail.com>
2020-08-15 18:43:53 +0530

    Added RecoverBinarySearchTree

--- recover_binary_search_tree/recover_binary_search_tree.java
+++ recover_binary_search_tree/recover_binary_search_tree.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package LeetcodeQuestionsSolvedJava.RecoverBinarySearchTree;
+package LeetcodeQuestionsSolvedJava.recover_binary_search_tree;
 
 import java.util.ArrayList;
 import java.util.List;
